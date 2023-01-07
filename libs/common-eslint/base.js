eab9922d910d3480f24e4cdb453e843c7a0562fd
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-01-07 08:37:47 -0300

    update

---
+++ libs/common-eslint/base.js
@@ -0,0 +1,44 @@
+module.exports = {
+  "env": {
+    "jest/globals": true,
+  },
+  "parser": "@typescript-eslint/parser",
+  "extends": [
+    "next",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "plugin:prettier/recommended",
+  ],
+  "plugins": ["@typescript-eslint"],
+  "rules": {
+    "import/no-default-export": "error",
+    "no-console": ["error", { "allow": ["warn", "error"] }],
+    "@next/next/no-img-element": "off",
+    "@next/no-html-link-for-pages": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }],
+  },
+  "overrides": [
+    {
+      "files": [
+        "pages/**/*.tsx",
+        "jest.config.{ts,js}",
+        "webpack.config.{ts,js}",
+        "next.config.{ts,js}",
+      ],
+      "rules": {
+        "import/no-default-export": "off",
+      }
+    },
+    {
+      "files": [
+        "webpack.config.{ts,js}",
+        "next.config.{ts,js}",
+      ],
+      "rules": {
+        "@typescript-eslint/no-var-requires": "off",
+        "no-console": "off",
+      },
+    }
+  ]
+}
