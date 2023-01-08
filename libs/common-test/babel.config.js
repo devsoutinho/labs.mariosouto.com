ee593ceab88d791403b6b0550751bc89af666312
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-01-08 13:32:18 -0300

    update

---
+++ libs/common-test/babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+}
+// module.exports = {
+//   "presets": [
+//     ["@babel/preset-react", {
+//       "runtime": "automatic"
+//     }],
+//     "@babel/preset-flow",
+//     "@babel/preset-env",
+//     "@babel/preset-typescript"
+//   ]
+// }
