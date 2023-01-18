7fa921e4d975681889ba2a7fb897409740ccb89f
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-01-18 10:23:41 -0300

    Update next.config.js (#26)

--- shells/web.labs.mariosouto.com/next.config.js
+++ shells/web.labs.mariosouto.com/next.config.js
@@ -12,6 +12,12 @@ const nextConfig = {
   async redirects() {
     return [
       {
+        source: "/links/eventos/imersao-dev-janeiro-2023",
+        destination:
+          "https://imersao.dev/?utm_source=influenciadores&utm_medium=devsoutinho&utm_campaign=imersaodevjan202",
+        permanent: true,
+      },
+      {
         source: "/challenges/001-css-preview-border-radius",
         destination:
           "https://devsoutinho.notion.site/CSS-Preview-Border-Radius-DevSoutinhoChallenges-14fd017db675495b81ce5cd5f68981f0",
