453382c85448eddbe5460c518650364bd13b8fca
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-01-19 08:21:16 -0300

    update

--- shells/web.labs.mariosouto.com/src/screens/DashboardScreen/DashboardScreen.tsx
+++ shells/web.labs.mariosouto.com/src/screens/DashboardScreen/DashboardScreen.tsx
@@ -1,4 +1,11 @@
-import { Box, Text, Link, useTheme } from "@devsoutinho/sknui/web";
+import {
+  Box,
+  Text,
+  Link,
+  useTheme,
+  Touchable,
+  Image,
+} from "@devsoutinho/sknui/web";
 
 const challenges = [
   {
@@ -9,6 +16,7 @@ const challenges = [
 
 export function DashboardScreen() {
   const { theme } = useTheme();
+  const maxWidth = "600px";
   return (
     <Box
       styleSheet={{
@@ -17,12 +25,73 @@ export function DashboardScreen() {
         alignItems: "center",
         justifyContent: "center",
         flex: 1,
+        padding: theme.spacing.x4,
         backgroundColor: theme.colors.neutral.x050,
       }}
     >
       <Box
         styleSheet={{
-          maxWidth: "800px",
+          width: "100%",
+          maxWidth: maxWidth,
+          alignSelf: "center",
+          justifyContent: "center",
+          borderRadius: theme.rounded.md,
+          boxShadow: theme.shadow.default,
+          padding: theme.spacing.x6,
+          marginBottom: theme.spacing.x6,
+          backgroundColor: theme.colors.neutral.x000,
+        }}
+      >
+        <Box
+          styleSheet={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Touchable href="/">
+            <Image
+              src="https://www.datocms-assets.com/85225/1673400260-logo-devsoutinho.png"
+              alt="Logo DevSoutinho"
+              styleSheet={{
+                borderRadius: theme.rounded.full,
+                width: theme.spacing.x20,
+                height: theme.spacing.x20,
+              }}
+            />
+          </Touchable>
+        </Box>
+        <Text
+          tag="h1"
+          typographyToken={{
+            xs: "heading2",
+            md: "heading1",
+          }}
+        >
+          Olá!
+        </Text>
+        <Text
+          styleSheet={{
+            marginTop: theme.spacing.x2,
+          }}
+        >
+          Você está acessando o #DevSoutinhoLabs no modo público, o que
+          significa que você pode ver os challenges, mas em breve para poder
+          colecionar conquistas e outras novidades vamos librar o acesso logado.
+          <br />
+          <br />
+          Por hora, convido você a fazer o challenge atual e participar do nosso
+          discord!
+          <br />
+          <br />
+        </Text>
+        <Link href="https://mariosouto.com/discord">Discord</Link>
+      </Box>
+      <Box
+        styleSheet={{
+          width: "100%",
+          maxWidth: maxWidth,
           alignSelf: "center",
           justifyContent: "center",
           borderRadius: theme.rounded.md,
