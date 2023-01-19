74b89e053574e96005e8ff10e864e794cb578157
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-01-19 08:09:06 -0300

    update

--- shells/web.labs.mariosouto.com/src/screens/DashboardScreen/DashboardScreen.tsx
+++ shells/web.labs.mariosouto.com/src/screens/DashboardScreen/DashboardScreen.tsx
@@ -12,29 +12,49 @@ export function DashboardScreen() {
   return (
     <Box
       styleSheet={{
-        flex: 1,
-        maxWidth: "400px",
-        alignSelf: "center",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
         justifyContent: "center",
+        flex: 1,
+        backgroundColor: theme.colors.neutral.x050,
       }}
     >
-      <Text tag="h1" typographyToken="heading1">
-        #DevSoutinhoChallenges
-      </Text>
       <Box
-        tag="ul"
         styleSheet={{
-          marginTop: theme.spacing.x6,
-          marginBottom: theme.spacing.x6,
+          maxWidth: "800px",
+          alignSelf: "center",
+          justifyContent: "center",
+          borderRadius: theme.rounded.md,
+          boxShadow: theme.shadow.default,
+          padding: theme.spacing.x6,
+          backgroundColor: theme.colors.neutral.x000,
         }}
       >
-        {challenges.map((challenge) => (
-          <Text tag="li" key={challenge.link}>
-            <Link href={challenge.link}>{challenge.title}</Link>
-          </Text>
-        ))}
+        <Text
+          tag="h1"
+          typographyToken={{
+            xs: "heading3",
+            md: "heading2",
+          }}
+        >
+          DevSoutinho Challenges
+        </Text>
+        <Box
+          tag="ul"
+          styleSheet={{
+            marginTop: theme.spacing.x6,
+            marginBottom: theme.spacing.x6,
+          }}
+        >
+          {challenges.map((challenge) => (
+            <Text tag="li" key={challenge.link}>
+              <Link href={challenge.link}>{challenge.title}</Link>
+            </Text>
+          ))}
+        </Box>
+        <Link href="/">Voltar ao login</Link>
       </Box>
-      <Link href="/">Voltar ao login</Link>
     </Box>
   );
 }
