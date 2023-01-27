7194bd3f4f04ce0d360d5d0423c87b7666f2f45b
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-01-26 22:26:41 -0300

    feat: add google analytics (#44)

--- shells/web.labs.mariosouto.com/pages/_app.tsx
+++ shells/web.labs.mariosouto.com/pages/_app.tsx
@@ -1,9 +1,27 @@
 import { Provider } from "@devsoutinho/sknui/web";
+import Script from "next/script";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
+const GA_ID = "G-6YZP7X6ZGP";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga-script">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', '${GA_ID}');
+      `}</Script>
+      <Head>
+        <title>DevSoutinho Labs</title>
+      </Head>
       <Provider>
         <Component {...pageProps} />
       </Provider>
