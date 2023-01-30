52e6195102313af8ee1d7311ff4c17a08992d5f8
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-01-30 19:46:31 -0300

    update

--- shells/web.labs.mariosouto.com/src/modules/workshops/repository.ts
+++ shells/web.labs.mariosouto.com/src/modules/workshops/repository.ts
@@ -1,13 +1,21 @@
 interface DatoCMSClientParams {
   query: string;
 }
-function datoCMSClient({ query }: DatoCMSClientParams) {
-  return fetch("https://graphql.datocms.com/", {
+
+const isPreview = process.env.NODE_ENV !== "production";
+
+function datoCMSClient({ query }: DatoCMSClientParams, preview = isPreview) {
+  const URL = preview
+    ? "https://graphql.datocms.com/preview"
+    : "https://graphql.datocms.com/";
+  const Authorization = `Bearer ${process.env.NEXT_PUBLIC_DATOCMS_KEY}`;
+
+  return fetch(URL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_DATOCMS_KEY}`,
+      Authorization,
     },
     body: JSON.stringify({
       query,
